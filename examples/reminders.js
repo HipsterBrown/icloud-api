6d739533a237a9650c06483cc3c503b86ff21840
HipsterBrown <headhipster@hipsterbrown.com>
2018-10-28 19:16:13 -0400

    docs(examples/reminders): update response format

--- examples/reminders.js
+++ examples/reminders.js
@@ -6,6 +6,48 @@ const {
 
 const cloud = new Cloud({ appleId: APPLE_ID, password: APPLE_PASSWORD })
 
+/*
+  Should return with JSON data like:
+
+  {
+    reminders: [{
+      guid,
+      pGuid,
+      etag,
+      lastModifiedDate,
+      createdDate,
+      createdDateExtended,
+      priority,
+      completedDate,
+      order,
+      title,
+      description,
+      dueDate,
+      dueDateIsAllDay,
+      startDate,
+      startDateIsAllDay,
+      startDateTz,
+      recurrence,
+      alarms,
+    }],
+    collections: [{
+      title,
+      guid,
+      ctag,
+      order,
+      color,
+      symbolicColor,
+      enabled,
+      emailNotification,
+      createdDate,
+      isFamily,
+      collectionShareType,
+      createdDateExtended,
+      participants,
+      completedCount,
+    }]
+  }
+*/
 cloud.reminders()
   .then(console.log)
   .catch(console.error)

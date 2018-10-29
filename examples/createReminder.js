7bae849199adb19a8fcdb05e7036c186360c7f1c
HipsterBrown <headhipster@hipsterbrown.com>
2018-10-29 11:41:51 -0400

    docs(examples/createReminder): update example with dueDate

--- examples/createReminder.js
+++ examples/createReminder.js
@@ -6,11 +6,16 @@ const {
 
 const cloud = new Cloud({ appleId: APPLE_ID, password: APPLE_PASSWORD })
 
+const date = new Date()
+date.setDate(date.getDate() + 1)
+
 // minimum object needed is a 'title' property
 // 'guid' will be created automatically if not provided
 // 'pGuid' (the guid of the Collection) will default to 'tasks' if not provided
 const reminder = {
-  title: 'This is an example reminder from my node module'
+  title: 'This is an example reminder from my node module',
+  dueDate: Cloud.formatICloudDate(date),
+  dueDateIsAllDay: true
 }
 
 cloud.createReminder(reminder)
